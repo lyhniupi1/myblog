147572f895c445e5c6749f6d2a4bc7ef5be9e450
e <k1003@qq.com>
2019-02-09 16:21:09 +0800

    wsgi myblog

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,6 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.flaskenv')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-from bluelog import create_app
+from myblog import create_app
 
 app = create_app('production')
